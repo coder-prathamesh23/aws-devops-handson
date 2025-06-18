ef21b4ed6dd425a89b12b0cfc82704a41b61f8fd
Prathamesh Naik <naikprathamesh2301@gmail.com>
2025-06-18 15:55:28 +0530

    fix: in Terraform Apply - It was supposed to get triggered when we push to the main but as we are using workflow_dispatch we push the Run Workflow UI Action. Hence needed to add an or condition in the Terraform Apply condition to get it triggered even on workflow_dispatch

--- terraform/ec2-instance/outputs.tf
+++ terraform/ec2-instance/outputs.tf
@@ -1,3 +1,5 @@
+# terraform/ec2-instance/outputs.tf
+
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance."
   value       = aws_instance.crecita_app.public_ip
@@ -9,11 +11,11 @@ output "instance_public_dns" {
 }
 
 output "ssh_command" {
-  description = "Command to SSH into the instance (replace ~/.ssh/your-key.pem with your actual key path)."
+  description = "Command to SSH into the instance."
   value       = "ssh -i ~/.ssh/${var.key_pair_name}.pem ec2-user@${aws_instance.crecita_app.public_ip}"
 }
 
 output "app_url" {
-  description = "URL to access the Crecita application on port 3000."
+  description = "URL to access the Crecita application."
   value       = "http://${aws_instance.crecita_app.public_ip}:3000"
 }
